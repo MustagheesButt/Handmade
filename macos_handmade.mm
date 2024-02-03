691968b52b3ba4165d8ea0f4e43f5d03424d44dd
Mustaghees Butt <renaldpirate@gmail.com>
2024-02-03 12:23:04 +0500

    feat(macOS-platform-layer): Day 3: backbuffer

--- macos_handmade.mm
+++ macos_handmade.mm
@@ -1,60 +1,86 @@
-#import <stdio.h>
-#import <AppKit/AppKit.h>
+#include <AppKit/AppKit.h>
 
-static float GlobalRenderingWidth = 1280;
-static float GlobalRenderingHeight = 720;
-static bool Running = true;
+#define internal static
+#define local_persist static
+#define global_variable static
 
-@interface HandmadeWindowDelegate: NSObject<NSWindowDelegate>;
+global_variable float globalRenderingWidth = 1280;
+global_variable float globalRenderingHeight = 720;
+global_variable bool running = true;
+global_variable uint8_t *buffer;
+
+@interface HandmadeMainWindowDelegate : NSObject <NSWindowDelegate>
+;
 @end
 
-@implementation HandmadeWindowDelegate
+@implementation HandmadeMainWindowDelegate
 
-- (void)windowWillClose: (NSNotification*) notification {
-  Running = false;
+- (void)windowWillClose:(id)sender {
+  running = false;
 }
 
 @end
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char *argv[]) {
+  HandmadeMainWindowDelegate *mainWindowDelegate =
+      [[HandmadeMainWindowDelegate alloc] init];
+
   NSRect screenRect = [[NSScreen mainScreen] frame];
-  NSRect windowRect = NSMakeRect(
-    (screenRect.size.width - GlobalRenderingWidth) * 0.5,
-    (screenRect.size.height - GlobalRenderingHeight) * 0.5,
-    GlobalRenderingWidth,
-    GlobalRenderingHeight
-  );
-  NSWindow* window = [[NSWindow alloc] initWithContentRect: windowRect
-                                       styleMask: NSWindowStyleMaskTitled |
-                                                  NSWindowStyleMaskClosable |
-                                                  NSWindowStyleMaskMiniaturizable |
-                                                  NSWindowStyleMaskResizable
-                                       backing: NSBackingStoreBuffered
-                                       defer: NO];
+  NSRect initialFrame =
+      NSMakeRect((screenRect.size.width - globalRenderingWidth) * 0.5,
+                 (screenRect.size.height - globalRenderingHeight) * 0.5,
+                 globalRenderingWidth, globalRenderingHeight);
+  NSWindow *window = [[NSWindow alloc]
+      initWithContentRect:initialFrame
+                styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
+                          NSWindowStyleMaskMiniaturizable |
+                          NSWindowStyleMaskResizable
+                  backing:NSBackingStoreBuffered
+                    defer:NO];
 
-  [window setBackgroundColor: NSColor.redColor];
-  [window setTitle: @"Handmade Hero"];
-  [window makeKeyAndOrderFront: nil];
+  [window setBackgroundColor:NSColor.blackColor];
+  [window setTitle:@"Handmade Hero"];
+  [window makeKeyAndOrderFront:nil];
+  [window setDelegate:mainWindowDelegate];
+  window.contentView.wantsLayer = YES;
 
-  HandmadeWindowDelegate* windowDelegate = [[HandmadeWindowDelegate alloc] init];
-  [window setDelegate: windowDelegate];
+  int bitmapWidth = window.contentView.bounds.size.width;
+  int bitmapHeight = window.contentView.bounds.size.height;
+  int bytesPerPixel = 4;
+  int pitch = bitmapWidth * bytesPerPixel; // size of a row
 
-  while (Running)
-  {
-    NSEvent* event;
-    do
-    {
-      event = [NSApp nextEventMatchingMask: NSEventMaskAny
-                untilDate: nil
-                inMode: NSDefaultRunLoopMode
-                dequeue: YES];
-      switch ([event type])
-      {
-        default:
-          [NSApp sendEvent: event];
-      }
+  buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+
+  while (running) {
+    @autoreleasepool {
+      NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc]
+          initWithBitmapDataPlanes:&buffer
+                        pixelsWide:bitmapWidth
+                        pixelsHigh:bitmapHeight
+                     bitsPerSample:8
+                   samplesPerPixel:bytesPerPixel
+                          hasAlpha:YES
+                          isPlanar:NO
+                    colorSpaceName:NSDeviceRGBColorSpace
+                       bytesPerRow:pitch
+                      bitsPerPixel:bytesPerPixel * 8] autorelease];
+
+      NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
+      NSImage *image = [[[NSImage alloc] initWithSize:imageSize] autorelease];
+      [image addRepresentation:rep];
+      window.contentView.layer.contents = image;
     }
-    while (event != nil);
+
+    NSEvent *event;
+    do {
+      event = [NSApp nextEventMatchingMask:NSEventMaskAny
+                                 untilDate:nil
+                                    inMode:NSDefaultRunLoopMode
+                                   dequeue:YES];
+      switch ([event type]) {
+      default:
+        [NSApp sendEvent:event];
+      }
+    } while (event != nil);
   }
 }
