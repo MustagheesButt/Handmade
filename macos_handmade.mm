61ea5a5e6e115db6da1c58dd85c624d6cc3fb3b0
Mustaghees Butt <renaldpirate@gmail.com>
2024-02-04 07:55:07 +0500

    feat(macos-platform-layer): animating the backbuffer + resize handling

--- macos_handmade.mm
+++ macos_handmade.mm
@@ -9,6 +9,72 @@ global_variable float globalRenderingHeight = 720;
 global_variable bool running = true;
 global_variable uint8_t *buffer;
 
+global_variable int bitmapWidth;
+global_variable int bitmapHeight;
+global_variable int bytesPerPixel = 4;
+global_variable int pitch;
+
+void macOS_refreshBuffer(NSWindow *window) {
+  if (buffer) {
+    free(buffer);
+  }
+
+  bitmapWidth = window.contentView.bounds.size.width;
+  bitmapHeight = window.contentView.bounds.size.height;
+  pitch = bitmapWidth * bytesPerPixel; // size of a row
+  buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+}
+
+void macOS_redrawBuffer(NSWindow *window) {
+  @autoreleasepool {
+    NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc]
+        initWithBitmapDataPlanes:&buffer
+                      pixelsWide:bitmapWidth
+                      pixelsHigh:bitmapHeight
+                   bitsPerSample:8
+                 samplesPerPixel:bytesPerPixel
+                        hasAlpha:YES
+                        isPlanar:NO
+                  colorSpaceName:NSDeviceRGBColorSpace
+                     bytesPerRow:pitch
+                    bitsPerPixel:bytesPerPixel * 8] autorelease];
+
+    NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
+    NSImage *image = [[[NSImage alloc] initWithSize:imageSize] autorelease];
+    [image addRepresentation:rep];
+    window.contentView.layer.contents = image;
+  }
+}
+
+global_variable int xOffset = 0;
+void renderWeirdGradiant() {
+  uint8_t *row = buffer;
+
+  for (int y = 0; y < bitmapHeight; y++) {
+    uint8_t *pixelChannel = row;
+
+    for (int x = 0; x < bitmapWidth; x++) {
+      // red
+      *pixelChannel = 0;
+      ++pixelChannel;
+
+      // green
+      *pixelChannel = x + xOffset;
+      ++pixelChannel;
+
+      // blue
+      *pixelChannel = y;
+      ++pixelChannel;
+
+      // alpha
+      *pixelChannel = 255;
+      ++pixelChannel;
+    }
+
+    row = pixelChannel;
+  }
+}
+
 @interface HandmadeMainWindowDelegate : NSObject <NSWindowDelegate>
 ;
 @end
@@ -17,6 +83,13 @@ global_variable uint8_t *buffer;
 
 - (void)windowWillClose:(id)sender {
   running = false;
+}
+
+- (void)windowDidResize:(NSNotification *)notification {
+  NSWindow *window = notification.object;
+  macOS_refreshBuffer(window);
+  renderWeirdGradiant(); // comment out for a beautiful effect when resizing
+  macOS_redrawBuffer(window);
 }
 
 @end
@@ -44,32 +117,12 @@ int main(int argc, const char *argv[]) {
   [window setDelegate:mainWindowDelegate];
   window.contentView.wantsLayer = YES;
 
-  int bitmapWidth = window.contentView.bounds.size.width;
-  int bitmapHeight = window.contentView.bounds.size.height;
-  int bytesPerPixel = 4;
-  int pitch = bitmapWidth * bytesPerPixel; // size of a row
-
-  buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+  macOS_refreshBuffer(window);
 
   while (running) {
-    @autoreleasepool {
-      NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc]
-          initWithBitmapDataPlanes:&buffer
-                        pixelsWide:bitmapWidth
-                        pixelsHigh:bitmapHeight
-                     bitsPerSample:8
-                   samplesPerPixel:bytesPerPixel
-                          hasAlpha:YES
-                          isPlanar:NO
-                    colorSpaceName:NSDeviceRGBColorSpace
-                       bytesPerRow:pitch
-                      bitsPerPixel:bytesPerPixel * 8] autorelease];
-
-      NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
-      NSImage *image = [[[NSImage alloc] initWithSize:imageSize] autorelease];
-      [image addRepresentation:rep];
-      window.contentView.layer.contents = image;
-    }
+    renderWeirdGradiant();
+    macOS_redrawBuffer(window);
+    xOffset += 1;
 
     NSEvent *event;
     do {
